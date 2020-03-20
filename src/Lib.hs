815f5de4b0f9bc66c988aee9be3e08b71472aeda
Paul <WildZes@gmail.com>
2020-03-20 21:43:31 +0300

    Added fetchJSON function to Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import           Network.HTTP.Simple      ( httpBS, getResponseBody )
+import qualified Data.ByteString.Char8 as BS
+
+url = "https://api.telegram.org/bot1117844810:AAFsVj2cgOHXxGeMCzin0ERJ_5HmshZSyiw/"
+
+fetchJSON :: IO BS.ByteString
+fetchJSON = do
+  res <- httpBS $ url ++ "getUpdates"
+  return (getRsponseBody res)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  json <- fetchJSON
+  BS.putStrLn json
